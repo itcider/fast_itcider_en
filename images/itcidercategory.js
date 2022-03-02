a017e7a1a9ea509892e799c5901d2cf7dae82ed2
itcider <80962780+itcider@users.noreply.github.com>
2022-03-02 15:27:51 +0900

    Add files via upload

--- images/itcidercategory.js
+++ images/itcidercategory.js
@@ -1,15 +1,15 @@
 /*
 skin name : fast itcider (en version)
-version : v2.2
+version : v3.0
 author : itcider.com
 website : itcider.com
 license : itcider license
 warning :
-Comments and code modifications are absolutely not allowed
+Comments cannot be edited or deleted
+fi skin phrase cannot be deleted
 An unlimited number of skins can be applied to the site
 Redistribution or unauthorized use of code is strictly prohibited
-Copyright notice at the bottom can be removed
-Distributable only on itcider official website
+Distributable only on the itcider official website
 If you do not follow the above precautions, you will not be able to use it.
 */
 function replaceLink() {
@@ -3741,15 +3741,15 @@ $(window).on('load', function () {
 
 /*
 skin name : fast itcider (en version)
-version : v2.2
+version : v3.0
 author : itcider.com
 website : itcider.com
 license : itcider license
 warning :
-Comments and code modifications are absolutely not allowed
+Comments cannot be edited or deleted
+fi skin phrase cannot be deleted
 An unlimited number of skins can be applied to the site
 Redistribution or unauthorized use of code is strictly prohibited
-Copyright notice at the bottom can be removed
-Distributable only on itcider official website
+Distributable only on the itcider official website
 If you do not follow the above precautions, you will not be able to use it.
 */
