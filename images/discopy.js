a59c32c9718c0a7a3ef923106b1348f9252b1049
itcider <80962780+itcider@users.noreply.github.com>
2022-03-10 13:08:56 +0900

    Update discopy.js

--- images/discopy.js
+++ images/discopy.js
@@ -17,7 +17,7 @@ If you do not follow the above precautions, you will not be able to use it.
       //=================================================================================
       // 우클릭, 드래그, 복사 차단https://gist.github.com/ab-c-d/7aa98987589466eee696952e154c98e1
       //=================================================================================
-      var events = ["contextmenu", "selectstart", "dragstart", "copy", "cut"];
+      var events = ["contextmenu", "selectstart", "dragstart", "cut"];
 
       events.forEach(function (event) {
           window.addEventListener(event, blockEvent, true);
